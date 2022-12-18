499e743ab507be4c496184ec00e640ea30645bed
rayan <rayanfayadhussein98@gmail.com>
2022-12-18 12:09:52 +0200

    Added my name to the app!

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
         <SayHi />
         <SayHello color="black" name={this.state.name} />
         <Search handleInput={this.handleInputChange} />
+        <p>Rayan Hussein</p>
       </div>
     );
   }
